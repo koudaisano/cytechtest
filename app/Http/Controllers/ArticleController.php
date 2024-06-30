0b6b67399969630a9a17d92a75f084ddf2a21074
koudaisano <koukuncreator@gmail>
2024-07-01 01:25:59 +0900

    2nd push

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,16 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
-
-class Controller extends BaseController
-{
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-}
 
 class ArticleController extends Controller
 {
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,11 +6,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Http\Request;
 
-class ArticleController extends Controller
+class Controller extends BaseController
 {
-    public function showList() {
-        return view('list');
-    }
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }

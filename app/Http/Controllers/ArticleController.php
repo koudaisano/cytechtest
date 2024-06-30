ecf333b821c8a48e88eaf3b6afab23727f12341b
koudaisano <koukuncreator@gmail>
2024-06-30 19:13:57 +0900

    Your commit message

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
+
+class ArticleController extends Controller
+{
+    public function showList() {
+        return view('list');
+    }
+}
---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function showList() {
+        return view('list');
+    }
+}
